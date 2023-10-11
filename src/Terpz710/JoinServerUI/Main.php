caccd52c7277e4dfb2f24a60412112cd44dcf9a1
Blueberry627 <145240944+Blueberry627@users.noreply.github.com>
2023-10-11 10:30:01 -0700

    Update Main.php

--- src/Terpz710/JoinServerUI/Main.php
+++ src/Terpz710/JoinServerUI/Main.php
@@ -27,16 +27,20 @@ class Main extends PluginBase implements Listener {
     public function sendJoinForm(Player $player) {
         $config = $this->getConfig()->get("messages");
 
+        $this->openForm($player, $config);
+    }
+
+    public function openForm(Player $player, array $config) {
         $form = new SimpleForm(function (Player $player, $data) use ($config) {
             if ($data !== null) {
                 if ($config["buttons"][$data] === "Yes") {
                     $this->playPopSound($player);
                     $this->sendTitle($player, $config["title_on_click"]);
                     $this->sendSubtitle($player, $config["subtitle_text"]);
-                } else {
-                    // Display the form again
-                    $this->sendJoinForm($player);
                 }
+            } else {
+                // If the form is closed without clicking anything, reopen it
+                $this->openForm($player, $config);
             }
         });
 
